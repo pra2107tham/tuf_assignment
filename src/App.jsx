21d5937c203ba6ceb650b945e6bce9ca8d262e41
Pratham Shirbhate <prathamshirbhate1909@gmail.com>
2024-08-12 00:45:23 +0530

    refactor: Update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import FlashCardlist from './components/FlashCardlist'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AdminDashboard from "./pages/AdminDashboard";
 const App = () => {
   return (
-    <div >
-      <Navbar />
-      <FlashCardlist/>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<AdminDashboard />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import FlashCardlist from '../components/FlashCardlist'
+
+const HomePage = () => {
+  return (
+    <div >
+      <Navbar />
+      <FlashCardlist/>
+    </div>
+  )
+}
+
+export default HomePage
